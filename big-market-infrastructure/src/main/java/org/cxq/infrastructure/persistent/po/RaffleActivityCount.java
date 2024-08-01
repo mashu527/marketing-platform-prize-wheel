7eeadd71aaa53b3826d85236235a49f32ce77742
MaShu <2293845083@qq.com>
2024-08-01 16:28:01 +0800

    feat：【营销服务】 第12节 用户参与抽奖活动库表设计

---
+++ big-market-infrastructure/src/main/java/org/cxq/infrastructure/persistent/po/RaffleActivityCount.java
@@ -0,0 +1,52 @@
+package org.cxq.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ *
+ * @description 抽奖活动次数配置表 持久化对象
+ * @create 2024-03-02 13:13
+ */
+@Data
+public class RaffleActivityCount {
+
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 活动次数编号
+     */
+    private Long activityCountId;
+
+    /**
+     * 总次数
+     */
+    private Integer totalCount;
+
+    /**
+     * 日次数
+     */
+    private Integer dayCount;
+
+    /**
+     * 月次数
+     */
+    private Integer monthCount;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
+
+
